977bba117984427d715dfe6cd8beb93f48f990f6
Funterr <aa.arsalan99@gmail.com>
2024-09-24 11:56:03 +0500

    new

---
+++ table/table.js.js
@@ -0,0 +1,12 @@
+const username=prompt("insert your value ")
+
+for( let i=1; i<=username; i++ ){
+
+    for (let j=1; j<=10; j++){
+
+        console.log(i+"x"+ j+"="+1 * j);
+            
+
+        }
+        
+}
